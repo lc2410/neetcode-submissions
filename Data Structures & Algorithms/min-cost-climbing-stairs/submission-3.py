a40299cc53ebbc7b7e9c85f5dac6d4e43c732c6f
Liang Chu <89421943+lc2410@users.noreply.github.com>
2026-08-30 16:49:19 -0400

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+        prev1 = cost[0]
+        prev2 = cost[1]
+
+        for i in range(2,n):
+            current = min(prev1, prev2) + cost[i]
+            prev1 = prev2
+            prev2 = current
+
+        return min(prev1, prev2)
